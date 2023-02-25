2a1bea272903eaf76c03c7ffe410f0e79f7afe65
Kali Soutar <kalisoutar@gmail.com>
2023-02-24 23:07:28 -0500

    Exercise 5

--- app/forms.py
+++ app/forms.py
@@ -1,8 +1,11 @@
 from flask_wtf import FlaskForm
-from wtforms import StringField, PasswordField
+from wtforms import StringField, PasswordField, FileField
 from wtforms.validators import InputRequired
-
+from flask_wtf.file import FileAllowed, FileRequired
 
 class LoginForm(FlaskForm):
     username = StringField('Username', validators=[InputRequired()])
     password = PasswordField('Password', validators=[InputRequired()])
+
+class UploadForm(FlaskForm):
+    photo = FileField('Photo',validators=[FileRequired(),FileAllowed(['png','jpg'],"Only upload images.")])
--- app/views.py
+++ app/views.py
@@ -4,7 +4,7 @@ from flask import render_template, request, redirect, url_for, flash, session, a
 from flask_login import login_user, logout_user, current_user, login_required
 from werkzeug.utils import secure_filename
 from app.models import UserProfile
-from app.forms import LoginForm
+from app.forms import LoginForm, UploadForm
 from werkzeug.security import check_password_hash
 
 
@@ -26,17 +26,24 @@ def about():
 
 
 @app.route('/upload', methods=['POST', 'GET'])
+@login_required
 def upload():
     # Instantiate your form class
+    form = UploadForm()
+    if request.method == 'POST':
 
-    # Validate file upload on submit
-    if form.validate_on_submit():
-        # Get file data and save to your uploads folder
+        # Validate file upload on submit
+        if form.validate_on_submit():
+            # Get file data and save to your uploads folder
+            photo = form.photo.data
+            pname=secure_filename(photo.filename)
+            photo.save(os.path.join(app.config['UPLOAD_FOLDER'],pname))
 
-        flash('File Saved', 'success')
-        return redirect(url_for('home')) # Update this to redirect the user to a route that displays all uploaded image files
 
-    return render_template('upload.html')
+            flash('File Saved', 'success')
+            return redirect(url_for('files')) # Update this to redirect the user to a route that displays all uploaded image files
+
+    return render_template('upload.html',form=form)
 
 
 @app.route('/login', methods=['POST', 'GET'])
@@ -50,7 +57,7 @@ def login():
         password = form.password.data
         
         # Get the username and password values from the form.
-        user = db.session.execute(db.select(UserProfile).filter_by(username=username))
+        user = db.session.execute(db.select(UserProfile).filter_by(username=username)).scalar()
         # Using your model, query database for a user based on the username
         # and password submitted. Remember you need to compare the password hash.
         # You will need to import the appropriate function to do so.
